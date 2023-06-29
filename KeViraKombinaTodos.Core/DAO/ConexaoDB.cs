92c70561132278991b3c67b288383df24a439351
guinago <aguinaldo_silvestre@hotmail.com>
2023-06-29 19:33:31 -0300

    Comitando conexão com azure

--- KeViraKombinaTodos.Core/DAO/ConexaoDB.cs
+++ KeViraKombinaTodos.Core/DAO/ConexaoDB.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
 
 namespace KeViraKombinaTodos.Core.DAO {
 	public class ConexaoDB {
 
 		public string mErro = "";
 		
-		private string ConexaoWebConfig = "SqlServer";
-		
-		private string Server = "localhost";
-		private string Database = "KeViraKombinaTodos";
-		private string Usuario = "autoMeGenerator";
-		private string Senha = "123456";
+		private string ConexaoWebConfig = "DefaultConnection";
 
-		public SqlConnection conn;
+        public SqlConnection conn;
 
 		public ConexaoDB(TipoConexao.Conexao TConexao) {
 			GetConexao(TConexao);
@@ -30,19 +27,27 @@ namespace KeViraKombinaTodos.Core.DAO {
 		// Faz a Conexao com o Banco de Dados
 		private void GetConexao(TipoConexao.Conexao TConexao) {
 			try {
-				string connectionStrings = "";
-				if (TConexao == TipoConexao.Conexao.Classe) {
-					connectionStrings = string.Format("Server={0},1433;Database={1};User ID={2};Password={3}", this.Server, this.Database, this.Usuario, this.Senha);
-				} else {
-					connectionStrings = getWebConfig(this.ConexaoWebConfig);
-				}
-
-				this.conn = new SqlConnection(connectionStrings);
+			string connectionStrings = "";
+				connectionStrings = getWebConfig(this.ConexaoWebConfig);
+                //connectionStrings = GetConexaoLocal();
+                this.conn = new SqlConnection(connectionStrings);
 			} catch (Exception erro) {
 				this.mErro = erro.Message;
 				this.conn = null;
 			}
 		}
+
+        private string GetConexaoLocal()
+        {
+            var server = "localhost";
+            var database = "KeViraKombinaTodos";
+            var usuario = "autoMeGenerator";
+            var senha = "123456";
+
+            var connectionStrings = string.Format("Server={0},1433;Database={1};User ID={2};Password={3}", server, database, usuario, senha);
+
+            return connectionStrings;
+        }
 
 		// Abre conexao com o Banco de Dados
 		public Boolean OpenConexao() {
@@ -61,23 +66,25 @@ namespace KeViraKombinaTodos.Core.DAO {
 		public void CloseConexao() {
 			conn.Close();
 			conn.Dispose();
-		}		
-		public string getWebConfig(string Variavel) {
-			string strValue = "";
-			//System.Configuration.Configuration rootWebConfig =
-			//		System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~/");
-			//System.Configuration.ConnectionStringSettings connString;
-			//if (0 < rootWebConfig.ConnectionStrings.ConnectionStrings.Count) {
-			//	connString = rootWebConfig.ConnectionStrings.ConnectionStrings[Variavel];
-			//	if (null != connString)
-			//		strValue = connString.ConnectionString;
-			//	else
-			//		strValue = "erro";
-			//}
-
-			return strValue;
 		}
-	}
+        public string getWebConfig(string Variavel)
+        {
+            string strValue = "";
+            Configuration rootWebConfig = null;
+            rootWebConfig = WebConfigurationManager.OpenWebConfiguration("~/");
+
+            ConnectionStringSettings connString;
+            if (0 < rootWebConfig.ConnectionStrings.ConnectionStrings.Count)
+            {
+                connString = rootWebConfig.ConnectionStrings.ConnectionStrings[Variavel];
+                if (null != connString)
+                    strValue = connString.ConnectionString;
+                else
+                    strValue = "erro";
+            }
+            return strValue;
+        }
+    }
 	public class TipoConexao {
 		public enum Conexao { WebConfig = 1, Classe = 2 };
 	}
